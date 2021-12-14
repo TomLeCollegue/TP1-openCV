69f50147a8a06514b1512de9fa4ba3d0bf70bd37
TomLeCollegue <tomkubasik.pro@gmail.com>
2021-12-14 15:14:08 +0100

    convert img to gray

--- minimum-prog.cpp
+++ minimum-prog.cpp
@@ -1,14 +1,28 @@
 // minimum-prog.cpp
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <stdlib.h>
 #include <iostream>
 
 using namespace cv;
+using namespace std;
  
 const String windowImage = "TP1";
 const String windowSlider = "Slider"; 
 const String nameSlider = "slider";
 const String imageToReadDefault = "lena.jpeg";
+
+Mat convertImgToGray(Mat mat) 
+{
+  Mat matOutGray;
+  if(mat.type() != CV_8UC1) {
+    cvtColor(mat, matOutGray, COLOR_RGB2GRAY);
+  } else {
+    cout << "Image déjà en noir et blanc" << endl;
+    matOutGray = mat;
+  }
+  return matOutGray;
+}
 
 int main(int argc,char *argv[])
 {
@@ -24,6 +38,11 @@ int main(int argc,char *argv[])
   createTrackbar( nameSlider, windowSlider, nullptr, 255,  NULL); // un slider
   setTrackbarPos( nameSlider, windowSlider, value );
   Mat f = imread(imageToRead);          // lit l'image "lena.png"
+// Convert to black and white 
+  f = convertImgToGray(f);
+  f = convertImgToGray(f)
+
+
   imshow( windowImage, f );                // l'affiche dans la fenêtre
   while ( waitKey(50) < 0 )            // attend une touche
   { // Affiche la valeur du slider
@@ -34,4 +53,4 @@ int main(int argc,char *argv[])
       std::cout << "value=" << value << std::endl;
     }
   }
-}               
+}          
