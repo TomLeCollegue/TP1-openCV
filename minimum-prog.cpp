3825be9a4051ad853a1a3c612e7e4e12783cf00e
TomLeCollegue <tomkubasik.pro@gmail.com>
2021-12-14 17:28:11 +0100

    added cumulate histogram

--- minimum-prog.cpp
+++ minimum-prog.cpp
@@ -3,6 +3,7 @@
 #include <opencv2/imgproc.hpp>
 #include <stdlib.h>
 #include <iostream>
+#include <numeric>
 
 using namespace cv;
 using namespace std;
@@ -13,31 +14,11 @@ const String windowHistogram = "Histogram";
 const String nameSlider = "slider";
 const String imageToReadDefault = "lena.jpeg";
 
-
-
 vector<double> histogramme(Mat image);
 vector<double> histogramme_cumule(const vector<double> &h_I);
-Mat afficheHistogrammes(const std::vector<double>& histogramme);
+Mat afficheHistogrammes(const std::vector<double> &histogramme);
 
 Mat convertImgToGray(Mat mat);
-
-void histogrammeOnWebCam(){
-    VideoCapture cap(0);
-    Mat img;
-    vector<double> hist;
-    Mat histMat;
-    
-    while (true) {
-        cap.read(img);
-        
-        hist = histogramme(img);
-        histMat = afficheHistogrammes(hist);
-        imshow("ImageWebcam", img);
-        imshow("histWebCam", histMat );
-        waitKey(1);
-        
-    }
-}
 
 vector<double> histogramme(Mat image)
 {
@@ -53,31 +34,72 @@ vector<double> histogramme(Mat image)
   return res;
 }
 
-
-Mat afficheHistogrammes(const vector<double>& histogramme) 
+vector<double> histogramme_cumule(const vector<double> &histogramme)
 {
-  Mat imageHist( 256, 256, CV_8UC1);
-  double max = *max_element(histogramme.begin(), histogramme.end());
-  cout<<"Max value: " << max << endl ;
+  vector<double> res(256);
+  double numberPixels = accumulate(histogramme.begin(), histogramme.end(), 0.0);
 
-  for (int line = 0; line < imageHist.rows; line++)
+  double currentValue = 0;
+
+  for (int i = 0; i < 256; i++)
   {
-    for (int column = 0; column < imageHist.cols; column++)
-    {
-      int newValueHistogram = ( histogramme[column] / max ) * 255;
-
-      int pixelValue;
-      if(newValueHistogram >= imageHist.rows - line) {
-        pixelValue = 0;
-      } else {
-        pixelValue = 255;
-      }
-      imageHist.at<uchar>(line,column) = pixelValue;
-    }
+    double newValueHistogram = (histogramme[i] / numberPixels) * 256;
+    currentValue += newValueHistogram;
+    res[i] = currentValue;
   }
-  return imageHist;
+
+  return res;
 }
 
+Mat afficheHistogrammes(const vector<double> &histogramme, const vector<double> &histogrammeCumule)
+{
+  Mat imageHist(256, 512, CV_8UC1);
+  
+  // Display Histogramme gauche
+  double max = *max_element(histogramme.begin(), histogramme.end());
+  cout << "Max value: " << max << endl;
+
+  for (int line = 0; line < 256; line++)
+  {
+    for (int column = 0; column < 256; column++)
+    {
+      int newValueHistogram = (histogramme[column] / max) * 256;
+      int pixelValue;
+      if (newValueHistogram >= 256 - line)
+      {
+        pixelValue = 0;
+      }
+      else
+      {
+        pixelValue = 255;
+      }
+      imageHist.at<uchar>(line, column) = pixelValue;
+    }
+  }
+
+// Display histogramme droit
+  for (int line = 0; line < 256; line++)
+  {
+    for (int column = 256; column < 512; column++)
+    {
+      int newValueHistogram = histogrammeCumule[column - 256];
+      int pixelValue;
+      if (newValueHistogram >= 255 - line)
+      {
+        pixelValue = 0;
+      }
+      else
+      {
+        pixelValue = 255;
+      }
+      imageHist.at<uchar>(line, column) = pixelValue;
+    }
+  }
+
+
+
+  return imageHist;
+}
 
 Mat convertImgToGray(Mat mat)
 {
@@ -119,13 +141,14 @@ int main(int argc, char *argv[])
   vector<double> hist;
   hist = histogramme(f);
 
+  vector<double> histCumul;
+  histCumul = histogramme_cumule(hist);
+
   Mat histMat;
-  histMat = afficheHistogrammes(hist);
+  histMat = afficheHistogrammes(hist, histCumul);
   imshow(windowHistogram, histMat); // l'affiche dans la fenêtre
 
   imshow(windowImage, f); // l'affiche dans la fenêtre
-
-  // histogrammeOnWebCam();
   while (waitKey(50) < 0) // attend une touche
   {                       // Affiche la valeur du slider
     int new_value = getTrackbarPos(nameSlider, windowSlider);
