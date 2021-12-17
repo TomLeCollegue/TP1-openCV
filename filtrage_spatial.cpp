3704395c1d15553fdad7de4e324c73c51870d095
TomLeCollegue <tomkubasik.pro@gmail.com>
2021-12-17 13:35:06 +0100

    add webcam support

--- filtrage_spatial.cpp
+++ filtrage_spatial.cpp
@@ -21,54 +21,83 @@ int main(int argc, char *argv[]) {
     setTrackbarPos(sliderSeuil, mainWindow, seuil);
     //----
 
-    String imageToReadName;
+    String imageToRead;
+    bool webcam = false;
     if (argc != 1) {
-        imageToReadName = argv[1];
+        if (std::string(argv[1]) == "webcam") {
+            webcam = true;
+        } else {
+            imageToRead = argv[1];
+        }
     } else {
-        imageToReadName = imageToReadDefault;
+        imageToRead = imageToReadDefault;
     }
-    Mat input = imread(imageToReadName); // lit l'image donnée en paramètre
 
-    if (input.channels() == 3)
-        cv::cvtColor(input, input, COLOR_BGR2GRAY);
+    int function = 'i';
+
+    //if (input.channels() == 3)
+    //    cv::cvtColor(input, input, COLOR_BGR2GRAY);
+
+    VideoCapture cap(0);
+    Mat input;
     while (true) {
+        cap.read(input);
+        cvtColor(input, input, COLOR_BGR2GRAY);
+
         int new_value = getTrackbarPos(nameSlider, mainWindow);
         if (alpha != new_value) {
             alpha = new_value;
             cout << "alpha= " << alpha << " %" << endl;
         }
 
+        int newSeuil = getTrackbarPos(sliderSeuil, mainWindow);
+        if (seuil != newSeuil) {
+            seuil = newSeuil;
+            cout << "Seuil= " << seuil << endl;
+        }
+
         int keycode = waitKey(50);
         int asciicode = keycode & 0xff;
         if (asciicode == 'q') {
             break;
-        } else if (asciicode == 'a') {
+        } else if (asciicode == 'a'
+                   || asciicode == 'm'
+                   || asciicode == 's'
+                   || asciicode == 'x'
+                   || asciicode == 'y'
+                   || asciicode == 'g'
+                   || asciicode == 'c'
+                   ) {
+            function = asciicode;
+        }
+
+        if (function == 'a') {
             // Q2
             input = filtreM(input);
             imshow(mainWindow, input); // l'affiche dans la fenêtre
-        } else if (asciicode == 'm') {
+        } else if (function == 'm') {
             // Q3
             cv::medianBlur(input, input, 3);
             imshow(mainWindow, input); // l'affiche dans la fenêtre
-        } else if (asciicode == 's') {
+        } else if (function == 's') {
             // Q4
             float alpha1 = (float) alpha / 100.0;
-            cout << "alpha : " << alpha1 << endl;
             input = contrasteM(input, alpha1);
             imshow(mainWindow, input); // l'affiche dans la fenêtre
-        } else if (asciicode == 'x') {
+        } else if (function == 'x') {
             input = sobelX(input);
             imshow(mainWindow, input); // l'affiche dans la fenêtre
-        } else if (asciicode == 'y') {
+        } else if (function == 'y') {
             input = sobelY(input);
             imshow(mainWindow, input); // l'affiche dans la fenêtre
-        } else if (asciicode == 'g') {
+        } else if (function == 'g') {
             input = gradient(input);
             imshow(mainWindow, input); // l'affiche dans la fenêtre
-        } else if (asciicode == 'c') {
+        } else if (function == 'c') {
             input = contour(input, seuil);
             imshow(mainWindow, input); // l'affiche dans la fenêtre
         }
+
         imshow(mainWindow, input);
     }
 
